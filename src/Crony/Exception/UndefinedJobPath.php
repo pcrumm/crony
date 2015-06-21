41c4e52f947551d864b504658c34a4e8c5874cbc
Phil Crumm <phil@pogoseat.com>
2015-06-21 16:25:13 -0700

    Add Exception that's fired when the specified Crony job path doesn't exist.

---
+++ src/Crony/Exception/UndefinedJobPath.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Crony
+ * @author Phil Crumm <pkcrumm@gmail.com>
+ * @license MIT
+ */
+namespace Crony\Exception;
+
+/**
+ * Thrown when the resolved path for Crony jobs does not exist.
+ */
+class UndefinedJobPath extends \Exception {
+
+}
